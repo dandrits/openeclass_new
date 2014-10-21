cd4c1256234ff42ceaa06712ac123e89233c322f
ellak <ellak@localhost.localdomain>
2014-10-21 22:04:36 +0300

    ex4

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-04 15:59
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '1234';
+$mysqlMainDb = 'eclass';
+$hackerEarthKey = 'ef34f0279e1af1fd967cbf7bd6562bece9e05041';
