dab81c76bd6881eff990c6b8700bf8d810b5e0e1
George Lazarou <grglaza@gmail.com>
2014-10-21 20:45:39 +0300

    Εργαστήριο 3ης εβδομάδας - Facebook API & Openeclass

--- modules/announcements/index.php
+++ modules/announcements/index.php
@@ -342,6 +342,8 @@ if ($is_editor) {
         else {
             $message = "<p class='success'>$langAnnAdd</p>";
         }
+        // Post to facebook.
+        postToFacebook($newContent, $id, $antitle, $course_code);
     } // end of if $submit
 
 
@@ -468,3 +470,66 @@ $head_content .= "<script type='text/javascript'>$(document).ready(function () {
 });</script>
 <link href='../../js/jquery.multiselect.css' rel='stylesheet' type='text/css'>";
 draw($tool_content, 2, null, $head_content);
+
+function postToFacebook($postContent, $postId, $postTitle, $courseCode) {
+  // Create the country codes array.
+  $countryCodes = array(
+    'GR',
+    'GB',
+    'FR',
+    // other codes go here.
+  );
+  // Init some other variables here.
+  $fields       = array();
+  $countriesStr = '';
+  // Facebook API call
+  $url = 'https://graph.facebook.com/v2.1/695730993849543/feed?access_token=CAANapFfgn3QBAA1reXj15nCo4RgZB3cEViKnXe0i0dTDnjhirBYYjVTv46sPL6sVosAR1L832I5wvlc3ObX4JCaZA8hubsW1qgEz0sS1bpuuDQKLZCAmMEY8guSz0BiNqQwEbpiSauM0wqwtW299p8BBzJUkTVtPMaJJNSCct3baXAwY1gy';
+  // Remove all html tags.
+  $postContent = strip_tags($postContent);
+  // Get all the words of the post content.
+  $newpostContentArr = explode(' ', $postContent);
+  // Iterate the words array.
+  foreach ($newpostContentArr as $key => $value) {
+    // Check if the first character is #.
+    $firstChar = substr($value, 0, 1);
+    if ($firstChar === '#') {
+      // Get the rest of the word.
+      $word = substr($value, 1);
+      // Check if the word exists in the country codes array.
+      if (in_array($word, $countryCodes) === true) {
+        $countriesStr .= $word.',';
+      }
+    }
+  }
+  if ($countriesStr !== '') {
+    // Remove last ',' character.
+    $countriesStr = rtrim($countriesStr, ',');
+    $fields['countries'] = $countriesStr;
+  }
+  // The message.
+  $fields['message'] = urlencode($postContent);
+  // The link.
+  $fields['link'] = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF'].'?course='.$courseCode.'&an_id='.$postId;
+  // The link title.
+  $fields['name'] = $postTitle;
+  // The link caption.
+  $fields['caption'] = ' ';
+  //url-ify the data for the POST
+  $fields_string = '';
+  foreach($fields as $key=>$value) {
+    $fields_string .= $key.'='.$value.'&';
+  }
+  // Remove last '&' character.
+  $fields_string = rtrim($fields_string, '&');
+  //open connection
+  $ch = curl_init();
+  //set the url, number of POST vars, POST data
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_POST, count($fields));
+  curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  //execute post
+  $result = curl_exec($ch);
+  //close connection
+  curl_close($ch);
+}
